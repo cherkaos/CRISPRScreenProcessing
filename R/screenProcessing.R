f4a61452ee7b0f086d0c88d530c4332f3d5bfabb
Sarah Cherkaoui <cherkaos@gmail.com>
2019-06-26 12:48:22 +0200

    Update screenProcessing.R

--- R/screenProcessing.R
+++ R/screenProcessing.R
@@ -13,25 +13,26 @@
 #'
 #' @examples
 screenProcessing<-function(inputfile,controlStart,controlEnd,resultfile,maxsgRNA,minReadCount,zscore){
-
+  
   if(!file.exists(inputfile)) stop('No such file "', inputfile,'"')
-
+  
   data=utils::read.csv(inputfile,sep = "\t", check.name=FALSE)
+  dataNotZscored=data
   if(zscore==TRUE){
     for(id in 3:length(data)){
       data[,id]=scale(data[,id],center=TRUE,scale=TRUE)[,1]
     }
   }
-
+  
   controlStartPos=c();
   controlEndPos=c();
   controlStartPos=match(controlStart,names(data))
   controlEndPos=match(controlEnd,names(data))
-
+  
   if(is.na(controlStartPos)) stop('The sample"', controlStart,'" is not part of samples "', names(data),'"')
   if(is.na(controlEndPos)) stop('The sample"', controlEndPos,'" is not part of samples "', names(data),'"')
-
-
+  
+  
   ALLfoldchange=c()
   output=data.frame()
   for(id in controlStartPos:controlEndPos){
@@ -40,8 +41,8 @@ screenProcessing<-function(inputfile,controlStart,controlEnd,resultfile,maxsgRNA
     xsorted <- data[order(x,decreasing = TRUE),c(1,2,id)]
     xsorted0=xsorted;
     #xsorted0=xsorted[xsorted[,3] != 0, ]
-
-
+    
+    
     for(j in 1:maxsgRNA)
     {
       foldchange=c(foldchange,xsorted0[j,3]/xsorted0[j+1,3])
@@ -49,7 +50,7 @@ screenProcessing<-function(inputfile,controlStart,controlEnd,resultfile,maxsgRNA
     ALLfoldchange=c(ALLfoldchange,max(foldchange))
     posFC=match(max(foldchange),foldchange)
     Newsorted=rbind(xsorted0[1:posFC,],Separation=c("-","-","-"),xsorted0[(posFC+1):(dim(xsorted0)[1]),])
-
+    
     #output=c(output,Newsorted)
     if(nrow(output)==0){
       output=Newsorted
@@ -58,14 +59,14 @@ screenProcessing<-function(inputfile,controlStart,controlEnd,resultfile,maxsgRNA
       output=cbind(output,Newsorted)
     }
   }
-
+  
   print('Hi Till, here are the FC calculated for your control clones :')
   print(ALLfoldchange)
   print('For the other clones, I will use the smalest FC as a threshold  :')
   print(min(ALLfoldchange))
   print('Which is control clone :')
   print(names(data)[match(min(ALLfoldchange),ALLfoldchange)+2])
-
+  
   refFoldchange=min(ALLfoldchange)
   clonesToRemove=c()
   position=controlEndPos+1;
@@ -73,6 +74,7 @@ screenProcessing<-function(inputfile,controlStart,controlEnd,resultfile,maxsgRNA
     foldchange=c()
     x=data[,i]
     xsorted =data[order(x,decreasing = TRUE),c(1,2,i)]
+    xsortedNotZscore=dataNotZscored[order(x,decreasing = TRUE),c(1,2,i)]
     if(!xsorted[1,3]==0){
       xsorted0=xsorted[xsorted[,3] != 0, ]
       #To find sudden drop
@@ -80,31 +82,31 @@ screenProcessing<-function(inputfile,controlStart,controlEnd,resultfile,maxsgRNA
       {
         foldchange=c(foldchange,xsorted0[j,3]/xsorted0[j+1,3])
       }
+
       posFC=match(max(foldchange),foldchange)
-      if(max(foldchange)>refFoldchange && xsorted0[posFC,3]>minReadCount){
-
-          Newsorted=rbind(xsorted0[1:posFC,],Separation=c("-","-","-"),xsorted0[(posFC+1):(dim(xsorted0)[1]),])
-
-          #output=c(output,Newsorted)
-          if(nrow(output)==0){
-            output=Newsorted
-          }
-          else{
-            output=cbind(output,Newsorted)
-          }
-
+      if(max(foldchange)>refFoldchange && xsortedNotZscore[posFC,3]>minReadCount){
+        
+        Newsorted=rbind(xsorted0[1:posFC,],Separation=c("-","-","-"),xsorted0[(posFC+1):(dim(xsorted0)[1]),])
+        
+        #output=c(output,Newsorted)
+        if(nrow(output)==0){
+          output=Newsorted
         }
+        else{
+          output=cbind(output,Newsorted)
+        }
+        
+      }
       else {
-           clonesToRemove=c(clonesToRemove,names(data)[i])
-        }
+        clonesToRemove=c(clonesToRemove,names(data)[i])
+      }
     }
     else {
-        clonesToRemove=c(clonesToRemove,names(data)[i])
+      clonesToRemove=c(clonesToRemove,names(data)[i])
     }
   }
-
+  
   # print file adding
   #newFile=sub(".txt","_Processed.txt",inputfile)
   utils::write.csv(output,resultfile, row.names = FALSE,na = " ",quote=F)
 }
-
